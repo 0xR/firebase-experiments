e5ded86fca00d69ff88b0ecdcde37f09dedfc183
Ruben Oostinga <ruben.oostinga@gmail.com>
2018-11-16 10:24:47 +0100

    Read and write to firestore

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyACG4clL2NUOduEBtKd2YnOkWVwn4H0vIE",
+  authDomain: "ruben-oostinga-speeltuin.firebaseapp.com",
+  databaseURL: "https://ruben-oostinga-speeltuin.firebaseio.com",
+  projectId: "ruben-oostinga-speeltuin",
+  storageBucket: "ruben-oostinga-speeltuin.appspot.com",
+  messagingSenderId: "390028126702"
+});
+
+const db = firebaseApp.firestore();
+
+db.collection("users")
+  .add({
+    first: "Alan",
+    middle: "Mathison",
+    last: "Turing",
+    born: 1912
+  })
+  .then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+    console.error("Error adding document: ", error);
+  });
+
+db.collection("collection")
+  .get()
+  .then(querySnapshot => {
+    querySnapshot.forEach(doc => {
+      console.log(`${doc.id} => `, doc.data());
+    });
+  });
 
 class App extends Component {
   render() {
